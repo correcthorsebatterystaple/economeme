f4c5e0c49fd093499b1a3eb5b0a657b6ac07cccb
Burhanuddin Salim <burhan.salman@gmail.com>
2021-10-31 12:07:26 +0000

    fix port

--- server/server.js
+++ server/server.js
@@ -85,8 +85,4 @@ app.all('*', (req, res) => {
   res.end();
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port, () => console.log(`Listening on Port ${port}...`));
+app.listen(process.env.PORT || 5000);
